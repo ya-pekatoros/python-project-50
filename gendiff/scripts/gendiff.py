5c3e55c34788d9a2434f4dec80164e1a9b84613e
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2022-12-18 07:52:14 +0300

    Add new optional argument in utility: "format" -f/--format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
 
+parser.add_argument("-f", "--format", help="set format of output",
+                    type=int)
+
 parser.add_argument("first_file")
 parser.add_argument("second_file")
 
